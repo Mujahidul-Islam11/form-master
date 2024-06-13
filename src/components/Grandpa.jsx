355cc97666cfb3c0516739de2d1a1a6cd18ed919
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-13 22:45:03 +0600

    Created a context to use data to multiple components without using props

--- src/components/Grandpa.jsx
+++ src/components/Grandpa.jsx
@@ -1,16 +1,53 @@
+import { createContext } from "react";
 import Aunty from "./Aunty";
 import Dad from "./Dad";
 import Uncle from "./Uncle";
 
+export const AssetContext = createContext("Gold");
 
 const Grandpa = () => {
-    return (
-        <div style={{border: "2px solid yellow", borderRadius: "8px", padding: "26px", display:"flex", gap: "20px", justifyContent: "center"}}>
-            <Dad asset={"Diamond"} style={{border: "2px solid yellow", borderRadius: "8px", padding: "12px"}}></Dad>
-            <Uncle style={{border: "2px solid yellow", borderRadius: "8px", padding: "12px"}}></Uncle>
-            <Aunty style={{border: "2px solid yellow", borderRadius: "8px", padding: "12px"}}></Aunty>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        border: "2px solid yellow",
+        borderRadius: "8px",
+        padding: "26px",
+        display: "flex",
+        gap: "20px",
+        justifyContent: "center",
+      }}
+    >
+      <AssetContext.Provider value="gold">
+        <Dad
+          asset={"Diamond"}
+          style={{
+            border: "2px solid yellow",
+            borderRadius: "8px",
+            padding: "12px",
+          }}
+        ></Dad>
+        <Uncle
+          style={{
+            border: "2px solid yellow",
+            borderRadius: "8px",
+            padding: "12px",
+          }}
+        ></Uncle>
+        <Aunty
+          style={{
+            border: "2px solid yellow",
+            borderRadius: "8px",
+            padding: "12px",
+          }}
+        ></Aunty>
+      </AssetContext.Provider>
+    </div>
+  );
 };
 
 export default Grandpa;
+
+/**
+ * 1. Create a context & export it
+ * 2. Add provider for the context with a value
+ */
--- src/components/Special.jsx
+++ src/components/Special.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable react/prop-types */
 
+import { useContext } from "react";
+import { AssetContext } from "./Grandpa";
+
 
 const Special = ({style, asset}) => {
+    const gift = useContext(AssetContext)
     return (
         <div style={style}>
             <h3>Special</h3>
             <p>Has: {asset}</p>
+            <p>Gift: {gift}</p>
         </div>
     );
 };
