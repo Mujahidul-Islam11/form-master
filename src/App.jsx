1c0537c84f2b13e998c65c96b68ba8765c4b97bc
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-13 11:10:50 +0600

    Created a component to create connection between em

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import "./App.css";
-import HookForm from "./components/HookForm";
-import RefForm from "./components/RefForm";
-import ReusableForm from "./components/ReusableForm";
-import Simple_form from "./components/Simple_form";
-import StateFull from "./components/StateFull";
+// import HookForm from "./components/HookForm";
+// import RefForm from "./components/RefForm";
+// import ReusableForm from "./components/ReusableForm";
+// import Simple_form from "./components/Simple_form";
+// import StateFull from "./components/StateFull";
 
 function App() {
-  const handleSingUp = (e) => {
-    e.preventDefault();
-    console.log(e.target.name.value, e.target.email.value, e.target.pass.value)
-  };
-  const handleUpdateProfile = (e) => {
-    e.preventDefault();
-    console.log(e.target.name.value, e.target.email.value, e.target.pass.value)
-  };
+  // const handleSingUp = (e) => {
+  //   e.preventDefault();
+  //   console.log(e.target.name.value, e.target.email.value, e.target.pass.value)
+  // };
+  // const handleUpdateProfile = (e) => {
+  //   e.preventDefault();
+  //   console.log(e.target.name.value, e.target.email.value, e.target.pass.value)
+  // };
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Form Master</h1>
       {/* <Simple_form></Simple_form> */}
       {/* <StateFull></StateFull> */}
       {/* <RefForm></RefForm> */}
       {/* <HookForm></HookForm> */}
-      <ReusableForm
+      {/* <ReusableForm
         title={"Sign Up"}
         handleSubmit={handleSingUp}
         submitText="Submit"
@@ -39,7 +39,7 @@ function App() {
         <div>
           <h1>Update your profile</h1>
         </div>
-      </ReusableForm>
+      </ReusableForm> */}
     </>
   );
 }
--- src/components/ReusableForm.jsx
+++ src/components/ReusableForm.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 
 
 const ReusableForm = ({title, handleSubmit, submitText="Submit", children}) => {
