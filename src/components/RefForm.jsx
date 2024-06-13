bd9fe10c07ed70acc5441d9e6596a88dda1cbcb8
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-13 10:04:32 +0600

    reference focus method for showing cursor in the input when page is rendered

--- src/components/RefForm.jsx
+++ src/components/RefForm.jsx
@@ -1,8 +1,12 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const RefForm = () => {
     const nameRef = useRef();
 
+    useEffect(()=>{
+        nameRef.current.focus() // shows cursor in the input when page is rendered
+    },[])
+    
     const handleSubmit = e =>{
         e.preventDefault();
         console.log(nameRef.current.value)
